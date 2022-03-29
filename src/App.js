fe76fd520f41b9f66edb4a9ba742fb82c92eaa84
gearymich <gearymich@gmail.com>
2022-03-29 11:44:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+  {
+    id: 1,
+    text: 'Doctors appointment',
+    day: '28.3.22',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Football game',
+    day: '5.4.22',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Dance recital',
+    day: '24.5.22',
+    reminder: true,
+  },
+  ])
+
+  const [people, setPeoples] = useState([
+    {
+      id: 4,
+      name: 'Doctors appointment',
+      dob: '28.3.22',
+      chosen: true,
+    },
+    {
+      id: 5,
+      name: 'Football game',
+      dob: '5.4.22',
+      chosen: false,
+    },
+    {
+      id: 6,
+      name: 'Dance recital',
+      dob: '24.5.22',
+      chosen: true,
+    },
+    ])
+  
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? 
+        { ...task, reminder: !task.reminder }
+        : task
+      )
+    )
+  }
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+    console.log(newTask);
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+
+    setTasks(tasks.filter(
+      (task) => task.id !== id)
+      )
+  }
+
+  const name = 'Michael'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='michael'
+        onAdd={() => setShowAddTask(!showAddTask)} 
+        showAdd={ showAddTask } 
+      />
+
+      {showAddTask && 
+        <AddTask onAdd={addTask} /> // short version of ternary
+      }
+      
+      <div className='body'>
+        <div className="column">
+          {tasks.length > 0 ? (
+            <Tasks tasks={tasks} 
+            onDelete={deleteTask}
+            onToggle={toggleReminder} />
+          ) : (
+            'No Tasks To Show'
+          )}
+        </div>
+
+        <div className="column">
+          {tasks.length > 0 ? (
+            <Tasks tasks={tasks} 
+            onDelete={deleteTask}
+            onToggle={toggleReminder} />
+          ) : (
+            'No Tasks To Show'
+          )}
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import Button from './Button'
+
+const onClick = () => {
+  console.log('click')
+}
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+      <h1> {title} </h1>
+      <Button 
+        color={showAdd ? 'red' : 'green'} 
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={ onAdd }/>
+    </header>
+  )
+}
+
+export default Header
